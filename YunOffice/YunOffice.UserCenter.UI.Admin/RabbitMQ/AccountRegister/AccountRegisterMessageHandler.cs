6f31c96c0248e416ff09413390c33f7c09ba463c
Bruce Chen <Chenxr@DESKTOP-0U3AGA6>
2016-07-14 18:34:44 +0800

    AccountRegisterMessageHandler

--- YunOffice/YunOffice.UserCenter.UI.Admin/RabbitMQ/AccountRegister/AccountRegisterMessageHandler.cs
+++ YunOffice/YunOffice.UserCenter.UI.Admin/RabbitMQ/AccountRegister/AccountRegisterMessageHandler.cs
@@ -1,15 +1,20 @@
-﻿using RabbitMQ.Client.Events;
-using System;
-using System.Text;
+﻿using YunOffice.UserCenter.BusnissLogic;
 using YunOffice.UserCenter.Entities;
 
 namespace YunOffice.UserCenter.UI.Admin.RabbitMQ.AccountRegister
 {
     public class AccountRegisterMessageHandler : MessageHandler<UserEntity>
     {
+        public UserBusnissLogic BusnissLogic { get; set; }
+
+        public AccountRegisterMessageHandler(UserBusnissLogic busnissLogic)
+        {
+            BusnissLogic = busnissLogic;
+        }
+
         public override void Hand(UserEntity message)
         {
-            
+            BusnissLogic.Save(message);
         }
     }
 }
