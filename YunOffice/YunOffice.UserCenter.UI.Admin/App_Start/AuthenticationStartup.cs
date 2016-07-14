611131e5e4572162d7dfd37f313b3abe8996c135
Bruce Chen <Chenxr@DESKTOP-0U3AGA6>
2016-07-14 18:19:04 +0800

    AuthenticationStartup

---
+++ YunOffice/YunOffice.UserCenter.UI.Admin/App_Start/AuthenticationStartup.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNet.Identity;
+using Microsoft.Owin;
+using Microsoft.Owin.Security.Cookies;
+using Owin;
+
+[assembly: OwinStartup(typeof(YunOffice.UserCenter.UI.Admin.App_Start.OwinStartup))]
+
+namespace YunOffice.UserCenter.UI.Admin.App_Start
+{
+    public partial class OwinStartup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            //app.UseCookieAuthentication(new CookieAuthenticationOptions
+            //{
+            //    AuthenticationType = DefaultAuthenticationTypes.ApplicationCookie,
+            //    LoginPath = new PathString("/Admin/Account/Login"),
+            //    CookieSecure = CookieSecureOption.SameAsRequest,
+            //    ExpireTimeSpan = System.TimeSpan.FromMinutes(120),
+            //});
+        }
+    }
+}
