4261f794fa12fa5c2af517ad18f3a53f5792c0ac
Bruce Chen <Chenxr@DESKTOP-0U3AGA6>
2016-07-14 17:14:29 +0800

    AccountController

---
+++ YunOffice/YunOffice.UserCenter.UI.Admin/Controllers/AccountController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Threading.Tasks;
+using YunOffice.UserCenter.BusnissLogic;
+
+namespace YunOffice.UserCenter.UI.Admin.Controllers
+{
+    public class AccountController : Controller
+    {
+        public UserBusnissLogic BusnissLogic { get; set; }
+
+        public AccountController(UserBusnissLogic busnissLogic)
+        {
+            BusnissLogic = busnissLogic;
+        }
+
+        [HttpGet]
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Login(string username, string password)
+        {
+            var logined = BusnissLogic.Login(username, password);
+
+            if (logined) return Redirect("/Home/Index/");
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Register(string displayname, string username, string password)
+        {
+            var success = BusnissLogic.Register(displayname, username, password);
+
+            if (success) return Redirect("/Home/Index/");
+            return View();
+        }
+    }
+}
