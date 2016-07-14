a1ed92876e74cbdcd17a85b32c3acbe120f501f2
Bruce Chen <Chenxr@DESKTOP-0U3AGA6>
2016-07-14 11:48:41 +0800

    YunOffice.UserCenter.Entities

---
+++ YunOffice/YunOffice.UserCenter.Entities/UserEntity.cs
@@ -0,0 +1,30 @@
+﻿using Dapper.Contrib.Extensions;
+using System;
+using YunOffice.Common.DataAccess;
+
+namespace YunOffice.UserCenter.Entities
+{
+    [Table("[dbo].[User]")]
+    public class UserEntity : IEntity
+    {
+        [Key]
+        public long Id { get; set; }
+        public long ApplicationId { get; set; }
+        public DateTime? CreateDate { get; set; }
+        public long? CreatorId { get; set; }
+        public string CreatorName { get; set; }
+        public DateTime? UpdateDate { get; set; }
+        public long? UpdaterId { get; set; }
+        public string UpdaterName { get; set; }
+
+        public string Account { get; set; }
+        public string Password { get; set; }
+        public string DisplayName { get; set; }
+
+        public long? DepartmentId { get; set; }
+        public string DepartmentName { get; set; }
+
+        public long? RoleId { get; set; }
+        public string RoleName { get; set; }
+    }
+}
