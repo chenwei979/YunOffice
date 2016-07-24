dbf06766e7b29cebce9b9d3008c3103767a9b837
Bruce-Windows <Bruce-Windows@DESKTOP-3G1HB9J>
2016-07-24 15:26:38 +0800

    GetInstance

--- YunOffice/YunOffice.UserCenter.UI.Admin/Infrastructure/AutofacControllerFactory.cs
+++ YunOffice/YunOffice.UserCenter.UI.Admin/Infrastructure/AutofacControllerFactory.cs
@@ -8,9 +8,16 @@ namespace YunOffice.UserCenter.UI.Admin.Infrastructure
 {
     public class AutofacControllerFactory : DefaultControllerFactory
     {
+        protected ILifetimeScope Container { get; set; }
+
+        public AutofacControllerFactory()
+        {
+            Container = AutofacContainerBuilder.Singleton.GetRootInstance().BeginLifetimeScope("AutofacWebRequest");
+        }
+
         protected override IController GetControllerInstance(RequestContext requestContext, Type controllerType)
         {
-            return controllerType == null ? null : (IController)AutofacContainerBuilder.Singleton.GetRootInstance().Resolve(controllerType);
+            return controllerType == null ? null : (IController)Container.Resolve(controllerType);
         }
     }
 }
