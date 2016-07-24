21f4dd4e0b8ee4870f41137236043cecb946d208
Bruce-Windows <Bruce-Windows@DESKTOP-3G1HB9J>
2016-07-24 17:06:32 +0800

    RedisDataAccess

---
+++ YunOffice/YunOffice.Common.Redis/RedisDataAccess.cs
@@ -0,0 +1,59 @@
+﻿using Newtonsoft.Json;
+using StackExchange.Redis;
+using System;
+
+namespace YunOffice.Common.Redis
+{
+    public class RedisDataAccess<TMessage>
+    {
+        protected ConnectionMultiplexer RedisManager { get; set; }
+
+        public RedisDataAccess(ConnectionMultiplexer redisManager)
+        {
+            RedisManager = redisManager;
+        }
+
+        public virtual TMessage Deserialize(string message)
+        {
+            return JsonConvert.DeserializeObject<TMessage>(message);
+        }
+
+        public virtual string Serialize(TMessage message)
+        {
+            return JsonConvert.SerializeObject(message);
+        }
+
+
+        public bool HasKey(string key)
+        {
+            return RedisManager.GetDatabase().KeyExists(key);
+        }
+
+        public TMessage Get(string key)
+        {
+            string value = RedisManager.GetDatabase().StringGet(key);
+            return Deserialize(value);
+        }
+
+        public bool Set(string key, TMessage message, int expireMinutes = 0)
+        {
+            var value = Serialize(message);
+            var db = RedisManager.GetDatabase();
+            if (expireMinutes > 0)
+            {
+                return db.StringSet(key, value, TimeSpan.FromMinutes(expireMinutes));
+            }
+            else
+            {
+                return db.StringSet(key, value);
+            }
+        }
+
+        public bool Remove(string key)
+        {
+            if (HasKey(key)) return RedisManager.GetDatabase().KeyDelete(key);
+
+            return true;
+        }
+    }
+}
---
+++ YunOffice/YunOffice.Common.Redis/RegisterModule.cs
@@ -0,0 +1,19 @@
+﻿using Autofac;
+using StackExchange.Redis;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace YunOffice.Common.Redis
+{
+    public class RegisterModule : Module
+    {
+        protected override void Load(ContainerBuilder builder)
+        {
+            var redisManager = ConnectionMultiplexer.Connect("localhost");
+            builder.RegisterInstance(redisManager).As<ConnectionMultiplexer>().SingleInstance();
+        }
+    }
+}
