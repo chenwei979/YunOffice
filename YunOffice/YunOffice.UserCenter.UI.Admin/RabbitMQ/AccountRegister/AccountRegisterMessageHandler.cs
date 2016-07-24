c3e274278f2408146e5bece8d2f730e2ddc22789
Bruce-Windows <Bruce-Windows@DESKTOP-3G1HB9J>
2016-07-24 17:13:26 +0800

    192.168.232.128

--- YunOffice/YunOffice.UserCenter.UI.Admin/RabbitMQ/AccountRegister/AccountRegisterMessageHandler.cs
+++ YunOffice/YunOffice.UserCenter.UI.Admin/RabbitMQ/AccountRegister/AccountRegisterMessageHandler.cs
@@ -2,6 +2,7 @@
 using System;
 using YunOffice.Common.AOP;
 using YunOffice.Common.RabbitMq;
+using YunOffice.Common.Redis;
 using YunOffice.UserCenter.BusnissLogic;
 using YunOffice.UserCenter.Entities;
 using YunOffice.UserCenter.UI.Admin.Models;
@@ -11,9 +12,11 @@ namespace YunOffice.UserCenter.UI.Admin.RabbitMQ.AccountRegister
     public class AccountRegisterMessageHandler : MessageHandler<AccountRegisterViewModel>
     {
         public UserBusnissLogic BusnissLogic { get; set; }
+        public RedisDataAccess<UserEntity> RedisDataAccess { get; set; }
 
-        public AccountRegisterMessageHandler(IMqConfig config) : base(config)
+        public AccountRegisterMessageHandler(IMqConfig config, RedisDataAccess<UserEntity> redisDataAccess) : base(config)
         {
+            RedisDataAccess = redisDataAccess;
         }
 
         [AccountRegisterMessageHandActionExecutor]
@@ -22,6 +25,7 @@ namespace YunOffice.UserCenter.UI.Admin.RabbitMQ.AccountRegister
             //System.Threading.Thread.Sleep(5000);
             var entity = EmitMapperFactory.Singleton.GetInstance<AccountRegisterViewModel, UserEntity>(message);
             BusnissLogic.Save(entity);
+            RedisDataAccess.Set(entity.Account, entity);
         }
     }
 
