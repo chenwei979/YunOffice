7f067b5cea1051f3fdf99958f431638a28636424
Bruce-Windows <Bruce-Windows@DESKTOP-3G1HB9J>
2016-07-24 12:57:10 +0800

    RegisterModule

---
+++ YunOffice/YunOffice.UserCenter.BusnissLogic/RegisterModule.cs
@@ -0,0 +1,12 @@
+﻿using Autofac;
+
+namespace YunOffice.UserCenter.BusnissLogic
+{
+    public class RegisterModule : Module
+    {
+        protected override void Load(ContainerBuilder builder)
+        {
+            builder.RegisterType<UserBusnissLogic>();
+        }
+    }
+}
---
+++ YunOffice/YunOffice.UserCenter.DataAccess/RegisterModule.cs
@@ -0,0 +1,12 @@
+﻿using Autofac;
+
+namespace YunOffice.UserCenter.DataAccess
+{
+    public class RegisterModule : Module
+    {
+        protected override void Load(ContainerBuilder builder)
+        {
+            builder.RegisterType<UserDataAccess>();
+        }
+    }
+}
